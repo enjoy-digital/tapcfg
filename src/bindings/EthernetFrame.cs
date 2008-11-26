4881e4aac2ccbca93f117dbbfb02430f395d7187
juhovh <juhovh@2f5d681c-ba19-11dd-a503-ed2d4bea8bb5>
2008-11-26 06:30:58 +0000

    Add more functionality to the EthernetFrame class
    git-svn-id: http://tapcfg.googlecode.com/svn/trunk@30 2f5d681c-ba19-11dd-a503-ed2d4bea8bb5

--- src/bindings/EthernetFrame.cs
+++ src/bindings/EthernetFrame.cs
@@ -1,16 +1,45 @@
 
+using System;
 
 namespace TAPCfg {
 	public class EthernetFrame {
 		private byte[] data;
+		private byte[] src = new byte[6];
+		private byte[] dst = new byte[6];
+		private int etherType;
 
 		public EthernetFrame(byte[] data) {
 			this.data = data;
+			Array.Copy(data, 0, dst, 0, 6);
+			Array.Copy(data, 6, src, 0, 6);
+			etherType = (data[12] << 8) | data[13];
 		}
 
 		public byte[] Data {
+			get { return data; }
+		}
+
+		public int Length {
+			get { return data.Length; }
+		}
+
+		public byte[] SourceAddress {
+			get { return src; }
+		}
+
+		public byte[] DestinationAddress {
+			get { return dst; }
+		}
+
+		public int EtherType {
+			get { return etherType; }
+		}
+
+		public byte[] Payload {
 			get {
-				return data;
+				byte[] ret = new byte[data.Length - 14];
+				Array.Copy(data, 14, ret, 0, ret.Length);
+				return ret;
 			}
 		}
 	}
