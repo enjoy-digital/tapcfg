341fc1d06cb1b799745c1438104cddd5648620c2
juhovh <juhovh@2f5d681c-ba19-11dd-a503-ed2d4bea8bb5>
2008-11-26 12:42:08 +0000

    Commit the TAPCfgTest.cs that was forgotten
    git-svn-id: http://tapcfg.googlecode.com/svn/trunk@42 2f5d681c-ba19-11dd-a503-ed2d4bea8bb5

---
+++ src/bindings/TAPCfgTest.cs
@@ -0,0 +1,17 @@
+
+using TAPCfg;
+using System.Net;
+
+namespace TAPCfg {
+	public class TAPCfgTest {
+		private static void Main(string[] args) {
+			EthernetDevice dev = new EthernetDevice();
+			dev.Start();
+			dev.SetAddress(IPAddress.Parse("192.168.1.1"), 16);
+			dev.SetAddress(IPAddress.Parse("fc00::1"), 64);
+			dev.Enabled = true;
+
+			System.Threading.Thread.Sleep(100000);
+		}
+	}
+}
