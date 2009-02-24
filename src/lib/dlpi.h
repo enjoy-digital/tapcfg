a4253da810c73121499a542851478ddb7ff32605
juhovh <juhovh@2f5d681c-ba19-11dd-a503-ed2d4bea8bb5>
2009-02-24 12:37:47 +0000

    Add copyright and information headers to the DLPI file
    git-svn-id: http://tapcfg.googlecode.com/svn/trunk@227 2f5d681c-ba19-11dd-a503-ed2d4bea8bb5

--- src/lib/dlpi.h
+++ src/lib/dlpi.h
@@ -1,3 +1,23 @@
+/**
+ *  tapcfg - A cross-platform configuration utility for TAP driver
+ *  Copyright (C) 2008-2009  Juho Vähä-Herttua
+ *
+ *  This library is free software; you can redistribute it and/or
+ *  modify it under the terms of the GNU Lesser General Public
+ *  License as published by the Free Software Foundation; either
+ *  version 2.1 of the License, or (at your option) any later version.
+ *
+ *  This library is distributed in the hope that it will be useful,
+ *  but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ *  Lesser General Public License for more details.
+ */
+
+/* This file includes the useful DLPI functions we need on Solaris
+ * to get and set the hardware address. For more information about
+ * DLPI on Solaris, there's a great website available at the address
+ * http://www.mm.kay-mueller.de/solaris_drivers.html. Without it
+ * writing these functions would've been much more painful */
 
 #include <string.h>
 #include <stropts.h>
