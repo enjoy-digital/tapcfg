7a4efbf09786317b6a3bd545240e829c693510bd
juhovh <juhovh@2f5d681c-ba19-11dd-a503-ed2d4bea8bb5>
2010-11-19 08:58:45 +0000

    Add some more comments related to Windows portability
    git-svn-id: http://tapcfg.googlecode.com/svn/trunk@348 2f5d681c-ba19-11dd-a503-ed2d4bea8bb5

--- src/include/tapcfg.h
+++ src/include/tapcfg.h
@@ -1,12 +1,19 @@
 #ifndef TAPCFG_H
 #define TAPCFG_H
 
+/* On Windows __stdcall calling convention is used and it is
+ * required on all callback functions to avoid segfault on
+ * stack cleanup. In fact all the API functions could use the
+ * __stdcall keyword as well, but the only benefit would be a
+ * smaller binary size with the expense of more code
+ */
 #if defined(_WIN32)
 # define TAPCFG_CALLBACK __stdcall
 #else
 # define TAPCFG_CALLBACK
 #endif
 
+/* On Windows we explicitly only export the API symbols */
 #if defined(_WIN32) && defined(DLL_EXPORT)
 # define TAPCFG_API __declspec(dllexport)
 #else
