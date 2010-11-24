84fe06123349510be19862635c92f4481546b480
juhovh <juhovh@2f5d681c-ba19-11dd-a503-ed2d4bea8bb5>
2010-11-24 17:56:19 +0000

    Add custom written SOURCES and autodefs.h to make the tap-win32 driver compile
    git-svn-id: http://tapcfg.googlecode.com/svn/trunk@364 2f5d681c-ba19-11dd-a503-ed2d4bea8bb5

---
+++ drivers/windows/tap-win32/autodefs.h
@@ -0,0 +1,32 @@
+#ifndef AUTODEFS_H
+#define AUTODEFS_H
+
+/*
+ * Minimum TAP-Win32 version number expected by userspace
+ *
+ * The TAP-Win32 version number is defined in tap-win32/SOURCES
+ */
+#define TAP_ID "tap901"
+#define TAP_WIN32_MIN_MAJOR 9
+#define TAP_WIN32_MIN_MINOR 1
+
+/* Friendly name for TAP driver */
+#define PRODUCT_TAP_DEVICE_DESCRIPTION "TAP-Win32 Adapter V9"
+
+/* Version number of DDK/WDK used to build TAP driver */
+#define DDKVER_MAJOR 7600
+
+/* Name of package */
+#define PACKAGE "tapcfg"
+
+/* Define to the full name of this package. */
+#define PACKAGE_NAME "TAPCfg"
+
+/* Define to the one symbol short name of this package. */
+#define PACKAGE_TARNAME "tapcfg"
+
+/* Define to the version of this package. */
+#define PACKAGE_VERSION "1.0.0"
+
+
+#endif
